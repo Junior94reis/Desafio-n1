6e931ada064c2bae89181d3c8d5680fca53a17ab
Junior94reis <99148640+Junior94reis@users.noreply.github.com>
2022-12-04 15:15:04 -0300

    primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+/*
+let compras = [ 
+    {feijao: 'branco',
+    arroz: 'parborizado',
+    acucar: 'mascavo'}
+];
+
+let total = compras.find(function(item) {
+    return (item.feijao == "branco")?true  :false ;
+
+})
+
+console.log(total)
+*/
